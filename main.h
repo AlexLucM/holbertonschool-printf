a5cbf7e59eb20019c456a77c3052eca4c5a1fa0f
Guillaume de Chilly <guillaumedechilly@gmail.com>
2025-11-27 22:32:26 +0000

    Update header and man page for _printf

--- main.h
+++ main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
 #include <stdarg.h>
+#include <unistd.h>
 
+/* Prototypes */
 int _printf(const char *format, ...);
 int handle_specifier(const char *format, int i, va_list args);
 int print_string(va_list args);
@@ -11,4 +12,4 @@ int print_char(va_list args);
 int print_i(va_list args);
 int print_d(va_list args);
 
-#endif
+#endif /* MAIN_H */
