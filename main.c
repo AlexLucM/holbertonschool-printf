2fc6e880253771e28b9165556e4b5f7c25bb775f
Guillaume de Chilly <guillaumedechilly@gmail.com>
2025-11-27 22:17:51 +0000

    Add test main.c for _printf

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - simple test for _printf
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _printf("Hello %s %d%% %c\n", "world", 42, 'A');
+    return (0);
+}
